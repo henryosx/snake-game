0f58a23ec772bb467b6aa55191b3d2328f08faaa
henryosx <raccoonjjang@naver.com>
2022-03-29 17:02:18 +0900

    delete some comments.

--- snake.py
+++ snake.py
@@ -1,6 +1,6 @@
 # import modules
 import pygame
-import random 
+import random
 from datetime import datetime, timedelta
 
 pygame.init() # initialize pygame
@@ -41,47 +41,37 @@ def draw_block(screen, color, position):
 
 class Snake:
     def __init__(self):
-        # 좌표(tuple)를 list자료형으로 저장하여 뱀의 위치를 저장한다.
-        self.positions = [(2,0),(1,0),(0,0)]  # 뱀의 위치
+        self.positions = [(2,0),(1,0),(0,0)]
         self.direction = ''
 
     def draw(self):
-        # for문을 이용해서 뱀의 위치를 그린다.
         draw_block(screen, RED, self.positions[0])
-        draw_block(screen, BLACK, self.positions[-1])
         for position in self.positions[1:-1]:
             draw_block(screen, GREEN, position)
+        draw_block(screen, BLACK, self.positions[-1])
 
     def move(self):
         head_position = self.positions[0]
         x, y = head_position
         if self.direction == 'N':
-            # 북쪽방향으로 이동하고 있기 때문에 머리의 북쪽방향에 하나를 추가하고 꼬리를 자른다.
             self.positions = [(x , y - 1)] + self.positions[:-1]
         elif self.direction == 'S':
-            # 남쪽방향으로 이동하고 있기 때문에 머리의 남쪽방향에 하나를 추가하고 꼬리를 자른다.
             self.positions = [(x , y + 1)] + self.positions[:-1]
         elif self.direction == 'W':
-            # 서쪽방향으로 이동하고 있기 때문에 머리의 서쪽방향에 하나를 추가하고 꼬리를 자른다.
             self.positions = [(x - 1, y)] + self.positions[:-1]
         elif self.direction == 'E':
-            # 동쪽방향으로 이동하고 있기 때문에 머리의 동쪽방향에 하나를 추가하고 꼬리를 자른다.
             self.positions = [(x + 1, y)] + self.positions[:-1]
 
     def grow(self):
         tail_position = self.positions[-1]
         x, y = tail_position
         if self.direction == 'N':
-            # 북쪽방향으로 이동하고 있기 때문에 꼬리의 남쪽방향에 하나를 추가한다.
             self.positions.append((x, y + 1))
         elif self.direction == 'S':
-            # 남쪽방향으로 이동하고 있기 때문에 꼬리의 북쪽방향에 하나를 추가한다.
             self.positions.append((x, y - 1))
         elif self.direction == 'W':
-            # 서쪽방향으로 이동하고 있기 때분에 꾜리의 동쪽방향으로 하나를 추가한다.
             self.positions.append((x + 1, y))
         elif self.direction == 'E':
-            # 동쪽방향으로 이동하고 있기 때문에 꼬리의 서쪽방향으로 하나를 추가한다.
             self.positions.append((x - 1, y))
 
 class Apple:
@@ -91,22 +81,18 @@ class Apple:
     def draw(self):
         draw_block(screen, RED, self.position)
 
-# 4. pygame 무한루프
 def runGame():
     global done, last_moved_time, score
-    #게임 시작 시, 뱀과 사과를 초기화
     snake = Snake()
     apple = Apple()
 
     while not done:
-        # 화면을 흰색으로 지운다.
         clock.tick(60)
         screen.fill(WHITE)
         # display the score.
-        text = font.render(f"score = {score}", True, BLUE) 
+        text = font.render(f"score = {score}", True, BLUE)
         screen.blit(text, (20, 0))
 
-        # 방향값을 받아서 이동방향을 결정한다.
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 done=True
@@ -114,22 +100,18 @@ def runGame():
                 if event.key in KEY_DIRECTION:
                     snake.direction = KEY_DIRECTION[event.key]
 
-        # 시간이 경과하면 뱀이 움직이도록한다.
         if timedelta(seconds=0.1) <= datetime.now() - last_moved_time:
             snake.move()
             last_moved_time = datetime.now()
 
-        # 뱀이 사과를 먹으면 뱀의 길이가 늘어나도록 한다.
         if snake.positions[0] == apple.position:
             score += 1
             snake.grow()
             apple.position = (random.randint(0, X_POS_MAX - 1), random.randint(0, Y_POS_MAX - 1))
 
-        # 뱀이 자신의 위치과 겹치면 종료한다.
         if snake.positions[0] in snake.positions[1:]:
             done = True
 
-        # 뱀이 경계를 넘어가면 종료한다.
         x,y = snake.positions[0]
         if x in range(0, X_POS_MAX) and y in range(0, Y_POS_MAX) and done != True:
             done = False
