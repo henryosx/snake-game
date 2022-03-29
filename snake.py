cd916cd6f87e78585efa318547dfd2585d24bcbc
henryosx <raccoonjjang@naver.com>
2022-03-30 08:45:16 +0900

    add draw_line

--- snake.py
+++ snake.py
@@ -11,11 +11,11 @@ BLOCK_SIZE = 20
 X_POS_MAX = int(SCREEN_SIZE_MAX / BLOCK_SIZE)
 Y_POS_MAX = int(SCREEN_SIZE_MAX / BLOCK_SIZE)
 
-WHITE = (255,255,255)
-RED = (255,0,0)
-GREEN = (0,255,0)
-BLUE = (0,0,255)
-BLACK = (0, 0, 0)
+WHITE   = (255,255,255)
+RED     = (255,  0,  0)
+GREEN   = (  0,255,  0)
+BLUE    = (  0,  0,255)
+BLACK   = (  0,  0,  0)
 
 pygame.display.set_caption("Snake Game!")
 screen = pygame.display.set_mode((SCREEN_SIZE_MAX, SCREEN_SIZE_MAX))
@@ -39,9 +39,16 @@ def draw_block(screen, color, position):
     block = pygame.Rect((position[0] * BLOCK_SIZE, position[1] * BLOCK_SIZE), (BLOCK_SIZE, BLOCK_SIZE))
     pygame.draw.rect(screen, color, block)
 
+# define to draw a line
+def draw_line(screen, color, start_position, end_position):
+    start_pos = [start_position[0] * BLOCK_SIZE, start_position[1] * BLOCK_SIZE]
+    end_pos = [end_position[0] * BLOCK_SIZE, end_position[1] * BLOCK_SIZE]
+    width = 1
+    pygame.draw.line(screen, color, start_pos, end_pos, width)
+
 class Snake:
     def __init__(self):
-        self.positions = [(2,0),(1,0),(0,0)]
+        self.positions = [(3,1),(2,1),(1,1)]
         self.direction = ''
 
     def draw(self):
@@ -81,7 +88,7 @@ class Snake:
         else:
             # check out of the boundary.
             x, y = self.positions[0]
-            if x in range(0, X_POS_MAX) and y in range(0, Y_POS_MAX):
+            if x in range(1, X_POS_MAX - 1) and y in range(1, Y_POS_MAX - 1):
                 return False
             else:
                 return True
@@ -94,7 +101,8 @@ class Apple:
         draw_block(screen, RED, self.position)
 
     def move(self):
-        self.position = (random.randint(0, X_POS_MAX - 1), random.randint(0, Y_POS_MAX - 1))
+        #self.position = (random.randint(0, X_POS_MAX - 1), random.randint(0, Y_POS_MAX - 1))
+        self.position = (random.randint(1, X_POS_MAX - 2), random.randint(1, Y_POS_MAX - 2))
 
 def runGame():
     global done, last_moved_time, score
@@ -103,7 +111,13 @@ def runGame():
 
     while not done:
         clock.tick(60)
+        # clear screen with white
         screen.fill(WHITE)
+        # draw box with red line
+        draw_line(screen, RED, [1, 1], [X_POS_MAX - 1, 1])
+        draw_line(screen, RED, [1, 1], [1, Y_POS_MAX - 1])
+        draw_line(screen, RED, [X_POS_MAX - 1, 1], [X_POS_MAX - 1, Y_POS_MAX - 1])
+        draw_line(screen, RED, [1, Y_POS_MAX - 1], [X_POS_MAX - 1, Y_POS_MAX - 1])
         # display the score.
         text = font.render(f"score = {score}", True, BLUE)
         screen.blit(text, (20, 0))
@@ -124,7 +138,7 @@ def runGame():
         if snake.positions[0] == apple.position:
             score += 1
             snake.grow()
-            apple.move() 
+            apple.move()
 
         # check a collision
         if snake.collision():
